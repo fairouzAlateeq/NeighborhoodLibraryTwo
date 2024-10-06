cc455bc434bb607ec93e509dd4c50b822547d6a2
Fai Alateeq <fairouzalatteq@gmail.com>
2024-10-06 14:53:44 -0500

    seocnd commit for sighbothood lin 2

---
+++ src/main/java/com/ps/Book.java
@@ -0,0 +1,77 @@
+package com.ps;
+
+import java.util.Scanner;
+
+public class Book{
+    int ID;
+    String isbn;
+    String title;
+    boolean isCheckedOut;
+    String checkedOutTo;
+
+    public Book(int ID, String isbn, String title, boolean isCheckedOut, String checkedOutTo) {
+        this.ID = ID;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = false;
+        this.checkedOutTo = "";
+    }
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Checked Out: " + isCheckedOut;
+    }
+
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+    public String checkedOut(String name){
+        return "";
+    }
+    public void checkIn(){
+
+    }
+    public static void exit(){
+        System.out.println("done");
+    }
+}
+
+
+// static Vehicle[] inventory = new Vehicle[20];
+
---
+++ src/main/java/com/ps/Main.java
@@ -0,0 +1,81 @@
+package com.ps;
+import java.util.Scanner;
+public class Main {
+
+    static Scanner scanny = new Scanner(System.in);
+    static Book[] inventory = new Book[20];
+
+    public static void main(String[] args) {
+
+        Book book = new Book (101121,"i dont know","Red",false,"Sami");
+        Book book1 = new Book(101122, "Learning Java", "Blue", true, "Alice");
+        Book book2 = new Book(101123, "Advanced Algorithms", "Green", false, "Bob");
+        Book book3 = new Book(101124, "Data Structures", "Yellow", true, "Charlie");
+        Book book4 = new Book(101125, "Software Design", "Purple", false, "David");
+        Book book5 = new Book(101126, "Operating Systems", "Orange", true, "Eve");
+
+        inventory[0] = book;
+        inventory[1] = book1;
+        inventory[2] = book2;
+        inventory[3] = book3;
+        inventory[4] = book4;
+        inventory[5] = book5;
+
+
+        System.out.println("Hello, what would you like to do?");
+        System.out.println("1. View Available books");
+        System.out.println("2. Show unavailable books:");
+        System.out.println("3. exit");
+
+        int mainMenuChoice = scanny.nextInt();
+
+        switch (mainMenuChoice){
+            case 1:
+                showAvailableBooks(inventory);
+                break;
+            case 2:
+                showCheckedOutBooks(inventory);
+                break;
+            case 3:
+                break;
+            default:
+                System.out.println("choose 1,2 or 3 honey");
+        }
+
+    }
+    public static void showAvailableBooks(Book[] books) {
+        for (Book book : inventory) {
+            if (book != null && !book.isCheckedOut())
+                System.out.println(book);
+        }
+        System.out.println("would you like to check out a book? 1 for yes 2 for no ");
+        int res = scanny.nextInt();
+        if (res == 1)
+        checkOutABook(inventory);
+    }
+       public static void showCheckedOutBooks(Book[] book) {
+           for (Book books : inventory)
+           if (books.isCheckedOut == true)
+           System.out.println(book);
+            }
+
+       public static void checkOutABook( Book[] book) {
+               showAvailableBooks(inventory);
+               System.out.println("what book");
+               String bookRes = scanny.nextLine();
+               System.out.println("You selected: " + bookRes);
+                for (Book item: inventory) {
+                    if (item != null && item.title.equalsIgnoreCase(bookRes)) {
+                        item.isCheckedOut = true;
+                        System.out.println("you checked: "+ item.title + item.isCheckedOut);
+                        System.out.println(" Wahts your name? ");
+                        String userName = scanny.nextLine();
+                        item.setCheckedOutTo(userName);
+                        break;
+                    }
+           }
+                // change that book ischeckout to true
+                // changed checkoutto to username
+            }
+
+}
